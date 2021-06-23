5806672e4895a74c556659d496c8b1d63c0acb7f
Konireddy's <sridhariparvathi@gmail.com>
2021-06-23 19:02:21 +0530

    Added PureFunctions.scala

---
+++ src/main/scala/com/practice/PureFunctions.scala
@@ -0,0 +1,19 @@
+package com.practice
+
+object PureFunctions {
+  def main(args: Array[String]): Unit = {
+
+    def square(a: Int) = {
+      var b: Int = a * a;
+      println("Square of the number is " + b);
+      println("Number is " + a);
+    }
+
+    square(4)
+  }
+
+    var l=List(2,4,6,7)
+   var l2= for(i<-l) yield i*i
+    println(l2)
+    println(l)
+}
