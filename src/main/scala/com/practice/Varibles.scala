4b02a9ed8d1c0f99759b6785acd791a86a2c7552
Konireddy's <sridhariparvathi@gmail.com>
2021-07-14 19:50:13 +0530

    Added variables.scala

---
+++ src/main/scala/com/practice/Varibles.scala
@@ -0,0 +1,13 @@
+package com.practice
+
+object Varibles extends App
+{
+       val a=10
+        val b=1.0
+        val c='@'
+        val d="sree"
+        val e=true
+  var a1=10;var b1=2.0;var c1='#';var d1="pra";var e1=false
+    println(a);println(b);println(c);println(d);println(e)
+  println(a1);println(b1);println(c1);println(d1);println(e1)
+}
